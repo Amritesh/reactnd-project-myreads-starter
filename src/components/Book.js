4345d8cc1b2a903839177d424ee434565cd0a9e1
Amritesh <thenectorgod@gmail.com>
2018-03-02 02:06:27 +0530

    linting

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,33 +1,33 @@
 import React, { Component } from "react";
 
 export default class Book extends Component {
-	constructor(props) {
-		super(props);
-		this.onChange = function(book, newValue) {
-			const { updateShelf } = this.props;
-			updateShelf(book,newValue);
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.onChange = function(book, newValue) {
+            const { updateShelf } = this.props;
+            updateShelf(book,newValue);
+        };
+    }
     
-	render() {
-		const { book } = this.props;
-		return (
-			<div className="book">
-				<div className="book-top">
-					<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks && book.imageLinks.thumbnail}")` }}></div>
-					<div className="book-shelf-changer">
-						<select value={book.shelf} onChange={(event) => this.onChange(book,event.target.value)}>
-							<option value="none" disabled>Move to...</option>
-							<option value="currentlyReading">Currently Reading</option>
-							<option value="wantToRead">Want to Read</option>
-							<option value="read">Read</option>
-							<option value="none">None</option>
-						</select>
-					</div>
-				</div>
-				<div className="book-title">{book.title}</div>
-				<div className="book-authors">{book.authors && book.authors.join(", ")}</div>
-			</div>
-		);
-	}
+    render() {
+        const { book } = this.props;
+        return (
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks && book.imageLinks.thumbnail}")` }}></div>
+                    <div className="book-shelf-changer">
+                        <select value={book.shelf} onChange={(event) => this.onChange(book,event.target.value)}>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors && book.authors.join(", ")}</div>
+            </div>
+        );
+    }
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -4,57 +4,57 @@ import * as BooksAPI from "../BooksAPI";
 import Book from "./Book";
 
 export default class Search extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			query: "",
-			list: []
-		};
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: "",
+            list: []
+        };
     
-		this.updateQuery = (query) => {
-			this.setState({ query: query });
+        this.updateQuery = (query) => {
+            this.setState({ query: query });
     
-			if (query) {
-				BooksAPI.search(query, 20).then((result) => {
-					if(!result.error){
-						const { books } = this.props;
-						const list = result.map((book) => {
-							var found = books.find(b => b.id === book.id);
-							if(found)
-								book.shelf = found.shelf;
-							else
-								book.shelf = "none";
-							return book;
-						});
-						this.setState({ list: list});
-					}
-					else
-						this.setState({ list: []});
-				});
-			}
-		};
-	}
-	render() {
-		const { updateShelf } = this.props;
-		const { query, list } = this.state;
+            if (query) {
+                BooksAPI.search(query, 20).then((result) => {
+                    if(!result.error){
+                        const { books } = this.props;
+                        const list = result.map((book) => {
+                            var found = books.find(b => b.id === book.id);
+                            if(found)
+                                book.shelf = found.shelf;
+                            else
+                                book.shelf = "none";
+                            return book;
+                        });
+                        this.setState({ list: list});
+                    }
+                    else
+                        this.setState({ list: []});
+                });
+            }
+        };
+    }
+    render() {
+        const { updateShelf } = this.props;
+        const { query, list } = this.state;
         
-		return (
-			<div className="search-books">
-				<div className="search-books-bar">
-					<Link className="close-search" to='/'>Close</Link>
-					<div className="search-books-input-wrapper">
-						<input type="text" placeholder="Search by title or author"
-							value={query} onChange={(event) => this.updateQuery(event.target.value)}/>
-					</div>
-				</div>
-				<div className="search-books-results">
-					<ol className="books-grid">
-						{list.length > 0 && list.map((book) => (
-							<Book key={book.id} book={book} updateShelf={updateShelf}/>
-						))}
-					</ol>
-				</div>
-			</div>
-		);
-	}
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link className="close-search" to='/'>Close</Link>
+                    <div className="search-books-input-wrapper">
+                        <input type="text" placeholder="Search by title or author"
+                            value={query} onChange={(event) => this.updateQuery(event.target.value)}/>
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid">
+                        {list.length > 0 && list.map((book) => (
+                            <Book key={book.id} book={book} updateShelf={updateShelf}/>
+                        ))}
+                    </ol>
+                </div>
+            </div>
+        );
+    }
 }
