cb918234dad123e8c16403245c2b5dfa436bd080
Amritesh <thenectorgod@gmail.com>
2018-03-02 01:57:56 +0530

    adding constructors

--- src/App.js
+++ src/App.js
@@ -1,58 +1,60 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
-import './App.css'
-import MyReads from './components/MyReads'
-import Search from './components/Search'
-import * as BooksAPI from './BooksAPI'
+import React from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import MyReads from "./components/MyReads";
+import Search from "./components/Search";
+import * as BooksAPI from "./BooksAPI";
 
 class BooksApp extends React.Component {
-  state = {
-    shelves: [{
-      id: 'currentlyReading',
-      name: 'Currently Reading',
-    },
-    {
-      id: 'wantToRead',
-      name: 'Want to Read',
-    },
-    {
-      id: 'read',
-      name: 'Read',
-    }],
-    books: []
-  }
-  
-  updateShelf = (book, newShelf) => {
-    const books = this.state.books;
-    book.shelf = newShelf;
-    BooksAPI.update(book, newShelf).then(() => {
-      if (newShelf === 'none'){
-        const nbooks = books.filter(b => b.id !== book.id);
-        this.setState({books: nbooks});
-      }else{
-        this.setState({books: books});
-      }
-    });
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			shelves: [{
+				id: "currentlyReading",
+				name: "Currently Reading",
+			},
+			{
+				id: "wantToRead",
+				name: "Want to Read",
+			},
+			{
+				id: "read",
+				name: "Read",
+			}],
+			books: []
+		};
+		this.updateShelf = (book, newShelf) => {
+			const books = this.state.books;
+			book.shelf = newShelf;
+			BooksAPI.update(book, newShelf).then(() => {
+				if (newShelf === "none"){
+					const nbooks = books.filter(b => b.id !== book.id);
+					this.setState({books: nbooks});
+				}else{
+					this.setState({books: books});
+				}
+			});
+		};
+	}
 
-  componentDidMount() {
-    BooksAPI.getAll().then((result) => {
-      this.setState({books: result});
-    });
-  }
+	componentDidMount() {
+		BooksAPI.getAll().then((result) => {
+			this.setState({books: result});
+		});
+	}
 
-  render() {
-    return (
-      <div className="app">
-        <Route exact path='/' render={() => (
-          <MyReads updateShelf={this.updateShelf} {...this.state}/>
-        )}/>
-        <Route exact path='/search' render={() => (
-          <Search updateShelf={this.updateShelf} books={this.state.books}/>
-        )}/>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="app">
+				<Route exact path='/' render={() => (
+					<MyReads updateShelf={this.updateShelf} {...this.state}/>
+				)}/>
+				<Route exact path='/search' render={() => (
+					<Search updateShelf={this.updateShelf} books={this.state.books}/>
+				)}/>
+			</div>
+		);
+	}
 }
 
-export default BooksApp
+export default BooksApp;
--- src/components/MyReads.js
+++ src/components/MyReads.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Shelf from "./Shelf";
 
 export default class MyReads extends Component {
-
-  filterShelf = (shelfId) => {
-    const { books } = this.props;
-    return books.filter(book => book.shelf === shelfId);
-  };
-
-  render() {
-    const { shelves, updateShelf } = this.props
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        {shelves.map((shelf) => (
-            <Shelf key={shelf.id} updateShelf={updateShelf} {...shelf} 
-            books={this.filterShelf(shelf.id)}/>
-        ))}
-        <div className="open-search">
-          <Link to='/search'>Add a book</Link>
-        </div>
-      </div>
-    )
-  }
+	constructor(props) {
+		super(props);
+		this.filterShelf = (shelfId) => {
+			const { books } = this.props;
+			return books.filter(book => book.shelf === shelfId);
+		};
+	}
+	render() {
+		const { shelves, updateShelf } = this.props;
+		return (
+			<div className="list-books">
+				<div className="list-books-title">
+					<h1>MyReads</h1>
+				</div>
+				{shelves.map((shelf) => (
+					<Shelf key={shelf.id} updateShelf={updateShelf} {...shelf} 
+						books={this.filterShelf(shelf.id)}/>
+				))}
+				<div className="open-search">
+					<Link to='/search'>Add a book</Link>
+				</div>
+			</div>
+		);
+	}
 }
