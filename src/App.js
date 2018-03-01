f3b92105325cff38b2444e7cfd761ffe989e71f7
Amritesh <thenectorgod@gmail.com>
2018-03-02 02:06:45 +0530

    linting

--- src/App.js
+++ src/App.js
@@ -6,55 +6,55 @@ import Search from "./components/Search";
 import * as BooksAPI from "./BooksAPI";
 
 class BooksApp extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			shelves: [{
-				id: "currentlyReading",
-				name: "Currently Reading",
-			},
-			{
-				id: "wantToRead",
-				name: "Want to Read",
-			},
-			{
-				id: "read",
-				name: "Read",
-			}],
-			books: []
-		};
-		this.updateShelf = (book, newShelf) => {
-			const books = this.state.books;
-			book.shelf = newShelf;
-			BooksAPI.update(book, newShelf).then(() => {
-				if (newShelf === "none"){
-					const nbooks = books.filter(b => b.id !== book.id);
-					this.setState({books: nbooks});
-				}else{
-					this.setState({books: books});
-				}
-			});
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            shelves: [{
+                id: "currentlyReading",
+                name: "Currently Reading",
+            },
+            {
+                id: "wantToRead",
+                name: "Want to Read",
+            },
+            {
+                id: "read",
+                name: "Read",
+            }],
+            books: []
+        };
+        this.updateShelf = (book, newShelf) => {
+            const books = this.state.books;
+            book.shelf = newShelf;
+            BooksAPI.update(book, newShelf).then(() => {
+                if (newShelf === "none"){
+                    const nbooks = books.filter(b => b.id !== book.id);
+                    this.setState({books: nbooks});
+                }else{
+                    this.setState({books: books});
+                }
+            });
+        };
+    }
 
-	componentDidMount() {
-		BooksAPI.getAll().then((result) => {
-			this.setState({books: result});
-		});
-	}
+    componentDidMount() {
+        BooksAPI.getAll().then((result) => {
+            this.setState({books: result});
+        });
+    }
 
-	render() {
-		return (
-			<div className="app">
-				<Route exact path='/' render={() => (
-					<MyReads updateShelf={this.updateShelf} {...this.state}/>
-				)}/>
-				<Route exact path='/search' render={() => (
-					<Search updateShelf={this.updateShelf} books={this.state.books}/>
-				)}/>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="app">
+                <Route exact path='/' render={() => (
+                    <MyReads updateShelf={this.updateShelf} {...this.state}/>
+                )}/>
+                <Route exact path='/search' render={() => (
+                    <Search updateShelf={this.updateShelf} books={this.state.books}/>
+                )}/>
+            </div>
+        );
+    }
 }
 
 export default BooksApp;
